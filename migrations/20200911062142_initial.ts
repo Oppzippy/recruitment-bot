1e8e30e26eba13e74e44f4db6f4b7c3feef13af2
Kyle Scheuing <oppzippy@gmail.com>
2020-09-14 20:20:58 -0400

    Only use one transaction per migration

--- migrations/20200911062142_initial.ts
+++ migrations/20200911062142_initial.ts
@@ -1,26 +1,24 @@
 import * as Knex from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-	await knex.schema.createTable("recruitment_invite_link", (table) => {
-		table.increments("id").primary();
-		table.string("guild_id").index().notNullable();
-		table.string("invite_link").unique().notNullable();
-		table.string("owner_discord_id").index().notNullable();
-		table.timestamps(true, true);
-	});
-
-	await knex.schema.createTable(
-		"accepted_recruitment_invite_link",
-		(table) => {
+	await knex.schema
+		.createTable("recruitment_invite_link", (table) => {
+			table.increments("id").primary();
+			table.string("guild_id").index().notNullable();
+			table.string("invite_link").unique().notNullable();
+			table.string("owner_discord_id").index().notNullable();
+			table.timestamps(true, true);
+		})
+		.createTable("accepted_recruitment_invite_link", (table) => {
 			table.increments("id").primary();
 			table.string("invite_link").index().notNullable();
 			table.string("acceptee_discord_id").unique().notNullable();
 			table.timestamps(true, true);
-		},
-	);
+		});
 }
 
 export async function down(knex: Knex): Promise<void> {
-	await knex.schema.dropTable("recruitment_invite_link");
-	await knex.schema.dropTable("accepted_recruitment_invite_link");
+	await knex.schema
+		.dropTable("recruitment_invite_link")
+		.dropTable("accepted_recruitment_invite_link");
 }
