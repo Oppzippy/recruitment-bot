53f9864b0782a8734c0577f1d4e8bfef07449f2e
Kyle Scheuing <oppzippy@gmail.com>
2020-10-04 20:18:13 -0400

    Fix duplicates being counted cross guild

--- migrations/20201004195141_invite-link-duplicate-view.ts
+++ migrations/20201004195141_invite-link-duplicate-view.ts
@@ -3,18 +3,23 @@ import * as Knex from "knex";
 export async function up(knex: Knex): Promise<void> {
 	await knex.raw(`
 		CREATE VIEW recruitment_invite_link_duplicates AS
-		SELECT invite_link, COUNT(*) AS duplicates
-		FROM accepted_recruitment_invite_link AS ril
-		WHERE ril.created_at > (
-			SELECT created_at FROM accepted_recruitment_invite_link AS rilc
-			WHERE rilc.acceptee_discord_id = ril.acceptee_discord_id
-			ORDER BY created_at ASC
+		SELECT ril.invite_link, COUNT(*) AS duplicates
+		FROM accepted_recruitment_invite_link AS aril
+		INNER JOIN recruitment_invite_link AS ril
+		ON ril.invite_link  = aril.invite_link
+		WHERE aril.created_at > (
+			SELECT aril2.created_at FROM accepted_recruitment_invite_link AS aril2
+			INNER JOIN recruitment_invite_link AS ril2
+			ON aril2.invite_link = ril2.invite_link
+			WHERE aril2.acceptee_discord_id = aril.acceptee_discord_id
+			AND ril.guild_id = ril2.guild_id
+			ORDER BY aril2.created_at ASC
 			LIMIT 1
 		)
-		GROUP BY ril.invite_link;
+		GROUP BY aril.invite_link;
 	`);
 }
 
 export async function down(knex: Knex): Promise<void> {
-	await knex.raw("DROP VIEW invite_link_duplicates");
+	await knex.raw("DROP VIEW recruitment_invite_link_duplicates");
 }
