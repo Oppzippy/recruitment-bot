cb05bc237d7d7992b5f409257250fe4013688ed7
Kyle Scheuing <oppzippy@gmail.com>
2020-10-23 07:48:23 -0400

    Update accepted invite link index created_at is used in the recruitment_invite_link_duplicates view. This should speed up that query so it doesn't have to do a full table scan.

---
+++ migrations/20201023074044_accepted-invite-link-index.ts
@@ -0,0 +1,33 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.alterTable(
+		"accepted_recruitment_invite_link",
+		(table) => {
+			table.dropIndex(
+				["invite_link", "acceptee_discord_id"],
+				"accepted_recruitment_invite_link_index",
+			);
+			table.index(
+				["invite_link", "acceptee_discord_id", "created_at"],
+				"accepted_recruitment_invite_link_index",
+			);
+		},
+	);
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.alterTable(
+		"accepted_recruitment_invite_link",
+		(table) => {
+			table.dropIndex(
+				["invite_link", "acceptee_discord_id", "created_at"],
+				"accepted_recruitment_invite_link_index",
+			);
+			table.index(
+				["invite_link", "acceptee_discord_id"],
+				"accepted_recruitment_invite_link_index",
+			);
+		},
+	);
+}
