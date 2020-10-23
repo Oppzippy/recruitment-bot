32b2fffeb7e457c26fe58ab4a74d76503da2b3ba
Kyle Scheuing <oppzippy@gmail.com>
2020-10-23 07:13:06 -0400

    Add accepted_recruitment_invite_link index

---
+++ migrations/20201023070900_accepted-invite-link-created-at-index.ts
@@ -0,0 +1,19 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.alterTable(
+		"accepted_recruitment_invite_link",
+		(table) => {
+			table.index("created_at");
+		},
+	);
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.alterTable(
+		"accepted_recruitment_invite_link",
+		(table) => {
+			table.dropIndex("created_at");
+		},
+	);
+}
