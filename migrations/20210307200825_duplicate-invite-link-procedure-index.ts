209f82834613d99d57d1a5c0849a1902f48461a7
Kyle Scheuing <kyle@kylescheuing.com>
2021-03-07 20:15:30 -0500

    Add index for DuplicateInvitesForUser procedure

---
+++ migrations/20210307200825_duplicate-invite-link-procedure-index.ts
@@ -0,0 +1,25 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.alterTable(
+		"accepted_recruitment_invite_link",
+		(table) => {
+			table.index(
+				["invite_link", "acceptee_discord_id", "id"],
+				"aril_duplicate_query_index",
+			);
+		},
+	);
+}
+
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.alterTable(
+		"accepted_recruitment_invite_link",
+		(table) => {
+			table.dropIndex(
+				["invite_link", "acceptee_discord_id", "id"],
+				"aril_duplicate_query_index",
+			);
+		},
+	);
+}
