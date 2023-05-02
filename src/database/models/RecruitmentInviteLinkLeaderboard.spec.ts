1808b34958781e1699e7db39373ab644f6f1d710
Kyle Scheuing <kyle@kylescheuing.com>
2023-05-02 18:10:27 -0400

    move test from integration to unit

---
+++ src/database/models/RecruitmentInviteLinkLeaderboard.spec.ts
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+import { parseFilter } from "./RecruitmentInviteLinkLeaderboard";
+
+describe("recruitment invite link leaderboard", () => {
+	it("handles null values in filters for end time properly", async function () {
+		const filter = parseFilter({
+			endDate: null,
+			resetIntervalInDays: null,
+			startDate: null,
+			userId: null,
+		});
+		expect(filter.endDate).to.be.undefined;
+		expect(filter.resetIntervalInDays).to.be.undefined;
+		expect(filter.startDate).to.be.undefined;
+		expect(filter.userId).to.be.undefined;
+	});
+});
